2d696f45b5a2ca70aabbe21838475379cc33cf78
Fasamii <fasami0512@gmail.com>
2025-11-11 19:18:30 +0100

    Added SwapchainConfig struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,34 @@
+#![allow(unused)]
+#![allow(dead_code)]
+
 use ash::{self, khr, vk};
 use std::ffi::CStr;
 
-pub struct Renderer {}
-impl Renderer {}
-
-pub struct RendererConfig<'a> {
-    app_info: vk::ApplicationInfo<'a>,
-    physical_device: PhysicalDeviceSelector,
+#[derive(Debug)]
+pub struct Base {}
+impl Base {
+    pub fn new(config: BaseConfig) -> Self {
+        Self {}
+    }
 }
 
-impl Default for RendererConfig<'_> {
+pub struct BaseConfig<'a> {
+    app_info: vk::ApplicationInfo<'a>,
+    physical_device: PhysicalDeviceSelector,
+    swapchain: SwapchainConfig,
+}
+
+impl Default for BaseConfig<'_> {
     fn default() -> Self {
         Self {
             app_info: Default::default(),
             physical_device: Default::default(),
+            swapchain: Default::default(),
         }
     }
 }
 
-impl<'a> RendererConfig<'a> {
+impl<'a> BaseConfig<'a> {
     pub fn with_app_info(mut self, name: &'a CStr, major: u32, minor: u32, patch: u32) -> Self {
         self.app_info = vk::ApplicationInfo::default()
             .application_name(name)
@@ -30,6 +40,59 @@ impl<'a> RendererConfig<'a> {
         physical_device_selector: fn(PhysicalDeviceSelector) -> PhysicalDeviceSelector,
     ) -> Self {
         self.physical_device = physical_device_selector(Default::default());
+        self
+    }
+    pub fn with_swapchain(
+        mut self,
+        swapchain_config: fn(SwapchainConfig) -> SwapchainConfig,
+    ) -> Self {
+        self.swapchain = swapchain_config(Default::default());
+        self
+    }
+}
+
+pub struct SwapchainConfig {
+    min_image_count: u32,
+    image_format: vk::Format,
+    image_sharing_mode: vk::SharingMode,
+    color_space: vk::ColorSpaceKHR,
+    present_mode: vk::PresentModeKHR,
+    image_usage: vk::ImageUsageFlags,
+    transforms: vk::SurfaceTransformFlagsKHR,
+    composite_alpha: vk::CompositeAlphaFlagsKHR,
+    array_layers: u32,
+    extent: vk::Extent2D,
+    clipped: bool,
+}
+
+impl Default for SwapchainConfig {
+    fn default() -> Self {
+        Self {
+            min_image_count: todo!(
+                "Try to acquire min img count supported by the swapchain and set that + 1"
+            ),
+            image_format: todo!("Set most common supported"),
+            image_sharing_mode: todo!("Set most common supported"),
+            color_space: todo!("same"),
+            present_mode: todo!("same"),
+            image_usage: todo!("same"),
+            transforms: todo!("same"),
+            composite_alpha: todo!("same"),
+            array_layers: todo!("same"),
+            extent: todo!("same"),
+            clipped: todo!("same"),
+        }
+    }
+}
+
+impl SwapchainConfig {
+    pub fn min_img_count(mut self, count: u32) -> Self {
+        self.min_image_count = count;
+        self
+    }
+
+    pub fn img_format(mut self, format: vk::Format) -> Self {
+        self.image_format = format;
         self
     }
 }
--- src/test.rs
+++ src/test.rs
@@ -1,11 +1,20 @@
+use ash::vk;
+
 fn main() {
-    let config = vinit::RendererConfig::default()
+    let config = vinit::BaseConfig::default()
         .with_app_info(c"TEST", 0, 0, 0)
-        .with_device(|selector| {
-            selector
+        .with_device(|physical_device_selector| {
+            physical_device_selector
                 .require_graphics_queue()
                 .require_compute_queue()
                 .require_transfer_queue()
+        })
+        .with_swapchain(|swapchain_config| {
+            swapchain_config
+                .min_img_count(12)
+                .img_format(vk::Format::R8G8B8A8_SRGB)
         });
-}
 
+    let base = vinit::Base::new(config);
+    println!("{base:?}");
+}
