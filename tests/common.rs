7ad57b88f91c7572c0378dce8097fadf3d475dcb
Fasamii <fasami0512@gmail.com>
2025-12-14 00:58:53 +0100

    small test tweaks

--- tests/common.rs
+++ tests/common.rs
@@ -12,10 +12,14 @@ where
         let _ = base.inspect_err(|err| {
             eprintln!("err = {err:?}");
         });
+        panic!("Some error (quick note: -- --nocapture (for showing output of test))");
     } else {
         println!("\x1b[38;5;2m [ OK ]::[Initialization was succesfull]\x1b[0m");
     }
 }
+
+// TODO: make create_base_instance, create_base_device, ... Then make test foos which asserts
+// values in created base
 
 #[test]
 fn create_empty() {
@@ -41,8 +45,24 @@ fn create_instance() {
 #[test]
 fn creating_device() {
     let base = vinit::BaseConfig::default()
-        .with(instance::Instance::default())
-        .with(device::Device::default())
+        .with(
+            instance::Instance::default()
+                .validation(vec![CString::new("VK_LAYER_KHRONOS_validation").unwrap()]),
+        )
+        .with(
+            device::Device::default()
+                .require_features(
+                    vk::PhysicalDeviceFeatures::default()
+                        .alpha_to_one(true)
+                        .occlusion_query_precise(true),
+                )
+                .require_properties(
+                    vk::PhysicalDeviceProperties::default().limits(
+                        vk::PhysicalDeviceLimits::default()
+                            .max_fragment_combined_output_resources(1235),
+                    ),
+                ),
+        )
         .build();
 
     check(base);
